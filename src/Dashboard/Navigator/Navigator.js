90acf2f23b0f482d330f79fd6fe6ee1bd3c05a51
Ahmadkashif <ahmadkashif983@gmail.com>
2021-05-18 18:38:07 +0500

    fix

--- src/Dashboard/Navigator/Navigator.js
+++ src/Dashboard/Navigator/Navigator.js
@@ -8,7 +8,6 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemIcon from '@material-ui/core/ListItemIcon';
 import ListItemText from '@material-ui/core/ListItemText';
-import HomeIcon from '@material-ui/icons/Home';
 import PeopleIcon from '@material-ui/icons/People';
 import DnsRoundedIcon from '@material-ui/icons/DnsRounded';
 import PermMediaOutlinedIcon from '@material-ui/icons/PhotoSizeSelectActual';
